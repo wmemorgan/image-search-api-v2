5c53bc662e8c52a8adb92cacbe5b90b30db2a0d5
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-05 19:34:55 +0800

    Add route error handling

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ require('dotenv').load()
 
 const Koa = require('koa')
 const Router = require('koa-router')
-//const serve = require('koa-static')
+const serve = require('koa-static')
+const fs = require('fs')
 
 
 const app = new Koa()
@@ -11,18 +12,17 @@ const port = process.env.PORT
 const apikey = process.env.APIKEY
 
 // Host static site
-// app.use(serve('./public'));
-// app.use(serve('./img'));
+app.use(serve('./public'));
+app.use(serve('./img'));
 
 router.get('/', ctx => ctx.body = 'Welcome to Imagesearch API')
-  // //404 Error Handling
-  // .get('/*', ctx => {
-  //   // ctx.status = 404
-  //   if (ctx.status === 404) {
-  //     ctx.status = 404
-  //     ctx.body = fs.readFileSync('./public/404.html', "utf8");
-  //   }
-  // })
+  //404 Error Handling
+  .get('/*', ctx => {
+    
+    if (ctx.status === 404) {
+      ctx.body = fs.readFileSync('./public/404.html', "utf8");
+    }
+  })
 
 app.use(router.routes())  
 app.listen(port, () => {
