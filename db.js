08264547bca8db8b052b095239737e24d288790d
Wilfred Morgan <wil.me.morgan@gmail.com>
2019-02-06 19:17:19 +0800

    Fix database setup function
    Fix database connection middleware

--- db.js
+++ db.js
@@ -2,38 +2,21 @@ const db = require('knex')({
   client: 'pg',
   connection: {
     connectionString: process.env.DBCONNECT,
-    ssl: true,
+    ssl: false,
   }
 })
 
-const setup = async (pg, schema) => {
-  await pg.transaction(async tx => {
-    const { drop, create } = schema
-    if (drop) {
-      for (const q of drop) {
-        await tx.query
-      }
-    }
-    if (create) {
-      for (const q of create) {
-        await tx.query(q)
-      }
-    }
-  })
+
+const setup = async (db, schema) => {
+  await schema(db)
 }
 
-const postgresMiddleware = (schema) => {
-  // const pg = new PgAsync({ connectionString: "postgres://postgres@localhost:5432/securedb" })
-  // const setupSchema = one(setup)
-  return async (err, ctx, next) => {
-    if (err) {
-      console.log("Unable to connect to the database", err);
-    } else {
-      await setup(db, schema)
-      console.log("Connection established");
-      ctx._postgres = db
-      return await next()
-    }
+const postgresMiddleware = (schema) => {  
+  return async (ctx, next) => {
+    await setup(db, schema)
+    console.log("Database connection established");
+    ctx._postgres = db
+    return await next()
   }
 }
 
@@ -41,7 +24,7 @@ const postgres = (ctx) => {
   return ctx._postgres
 }
 
-module.exports = { postgresMiddleware, postgres, setup }
+module.exports = { postgresMiddleware, postgres }
 
 
 
