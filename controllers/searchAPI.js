895e0187ecac48ba5e0699a7f9a39364b776efcf
Wilfred Morgan <wil.me.morgan@gmail.com>
2020-09-09 15:58:54 +0800

    Assign unique IDs to search results

--- controllers/searchAPI.js
+++ controllers/searchAPI.js
@@ -1,28 +1,30 @@
 // Google Search API
-const { google } = require('googleapis')
-const apikey = process.env.APIKEY
-const customsearch = google.customsearch('v1')
+const { google } = require("googleapis");
+const apikey = process.env.APIKEY;
+const customsearch = google.customsearch("v1");
+const { v4: uuidv4 } = require("uuid");
 
 const imageSearch = async (search, offset) => {
-  const results = await customsearch.cse.list({
-    cx: '008245539995824095644:3f27vg6irlc',
-    q: search,
-    auth: apikey,
-    searchType: 'image',
-    start: offset,
-  })
+	const results = await customsearch.cse.list({
+		cx: "008245539995824095644:3f27vg6irlc",
+		q: search,
+		auth: apikey,
+		searchType: "image",
+		start: offset,
+	});
 
-  const searchList = results.data.items
-  const displayItems = searchList.map(item => {
+	const searchList = results.data.items;
+	const displayItems = searchList.map((item) => {
     let itemDetails = {
-      url: item.link,
-      snippet: item.snippet,
-      thumbnail: item.image.thumbnailLink,
-      context: item.image.contextLink
-    }
-    return itemDetails
-  })
-  return displayItems
-}
+      id: uuidv4(),
+			url: item.link,
+			snippet: item.snippet,
+			thumbnail: item.image.thumbnailLink,
+			context: item.image.contextLink,
+		};
+		return itemDetails;
+	});
+	return displayItems;
+};
 
-module.exports = { imageSearch }
+module.exports = { imageSearch };
